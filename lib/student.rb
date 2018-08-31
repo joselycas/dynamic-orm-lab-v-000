e4b9a4bcc993789e6090e717f481046de847f65b
Jos Castro <joselycas@gmail.com>
2018-08-31 14:19:01 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,6 +66,6 @@ class Student < InteractiveRecord
  def self.find_by(value)
    value = hash.values.first
    sql = "SELECT * FROM #{self.table_name} WHERE ? = '#{value}'"
-   DB[:conn].execute(sql, value)
+   DB[:conn].execute(sql)
  end
 end
