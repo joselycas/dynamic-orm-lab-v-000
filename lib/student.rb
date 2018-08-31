8146ae4eeeda571ce713e37ab1b50b64d28cf40e
Jos Castro <joselycas@gmail.com>
2018-08-31 13:34:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,10 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
+
   def self.table_name
     self.to_s.downcase.pluralize
   end
@@ -27,9 +31,5 @@ class Student < InteractiveRecord
     options.each do |property, value|
     self.send("#{property}=", value)
    end
-  end
-
-  self.column_names.each do |col_name|
-    attr_accessor col_name.to_sym
   end
 end
