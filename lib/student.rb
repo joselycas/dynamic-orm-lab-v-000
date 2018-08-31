743f9345a7c71dc67c209009cd91ce8bac104ac5
Jos Castro <joselycas@gmail.com>
2018-08-31 13:38:58 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,4 +32,8 @@ class Student < InteractiveRecord
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
   end
+
+  def table_name_for_insert
+   self.class.table_name
+  end
 end
